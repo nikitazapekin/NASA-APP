2d1c8d61ef686b1135af18390a12c935f991ed14
Nikita <wotblitz362@mail.ru>
2023-10-06 11:52:18 +0300

    par

--- src/components/moonPics/moonPics.tsx
+++ src/components/moonPics/moonPics.tsx
@@ -4,7 +4,10 @@ import Moon2 from "../../asserts/space2.jpg"
 import Moon3 from "../../asserts/space3.jpg"
 import Moon4 from "../../asserts/space4.jpg"
 import Moon5 from "../../asserts/space5.jpg"
+import { useParallax } from "react-scroll-parallax"
 const MoonPics = () => {
+
+ 
     return (  
         <div className="moonPics">
 <img className="moonPic" alt="moon" src={Moon3} />
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -8,71 +8,17 @@ import "./pagesStyles.scss"
 import Moon from "../asserts/moon.png"
 import MoonPics from "../components/moonPics/moonPics";
 import { ParallaxBanner } from "react-scroll-parallax";
+import ParallaxElems from "../components/parallaxElems/parallaxElems";
 interface HomepageProps {
     isAuthenticated: boolean;
   }
- /* interface parallaxProps {
-    ref: React.RefObject<HTMLElement>;
-   controller: ParallaxController | null;
-   //constroller: any;
-    element: Element | undefined;
-  } */
-const Homepage=({isAuthenticated}: HomepageProps)=> {
-     
  
-
-
-      const parallax: any = useParallax({
-     //   easing: [1, -0.75, 0.5, 1.34],
-        translateX: [-100, 1100],
-        translateY: [-1500, 300],
-      }); 
-
-      const parallax1: any = useParallax({
-        //   easing: [1, -0.75, 0.5, 1.34],
-           translateX: [600, -100],
-           translateY: [100, 500]
-          // translateY: [-100, 1200],
-         }); 
-    
-    return (
-        
+const Homepage=({isAuthenticated}: HomepageProps)=> {
+    return ( 
         <div className="pageLayout">
-       
-
-       
         <Navigation isAuthenticated={isAuthenticated} />
-<div className="container-body">
-
-  <div ref={parallax1.ref} className="moonEffect">
-    <img src={Moon} alt="moon" className="parallaxMoon" />
-  </div>
-     <div className="box" ref={parallax.ref}>
-        <img src={Rocket } alt="rocket" className="rocket"   />
-  </div> 
-   {/* <div className="box2" ref={parallax1.ref}>
-        REPLAY
-    </div>   */}
-    </div>
-
-
-
-
-  
+  <ParallaxElems />
     <MoonPics />  
-
-
-
-
-
-
-
-
-
-
-
-
-
         <Footer />
 
         </div>
